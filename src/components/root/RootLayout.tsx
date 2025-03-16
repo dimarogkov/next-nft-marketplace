048251c8db04e06ec2a35c42c78d7f3a37c1e93b
dima.rogkov <dimarogkov41475@ukr.net>
2025-03-16 13:13:51 +0200

    some fix

--- src/components/root/RootLayout.tsx
+++ src/components/root/RootLayout.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, Suspense } from 'react';
 import { Footer, Header } from '../blocks';
 import Lenis from './Lenis';
 
@@ -8,11 +8,13 @@ type Props = {
 
 const RootLayout: FC<Props> = ({ children }) => {
     return (
-        <Lenis>
-            <Header />
-            <main className='relative flex flex-col grow w-full pt-[70px] sm:pt-20 lg:pt-[100px]'>{children}</main>
-            <Footer />
-        </Lenis>
+        <Suspense fallback={null}>
+            <Lenis>
+                <Header />
+                <main className='relative flex flex-col grow w-full pt-[70px] sm:pt-20 lg:pt-[100px]'>{children}</main>
+                <Footer />
+            </Lenis>
+        </Suspense>
     );
 };
 
