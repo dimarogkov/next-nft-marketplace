641cc0ee30f80c9c14acd927f550f132b69ac9a1
dima.rogkov <dimarogkov41475@ukr.net>
2025-06-02 19:23:41 +0300

    add some styles for Toast in mobile

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -1,5 +1,5 @@
 import toast, { type Toast } from 'react-hot-toast';
-import { FC, HTMLAttributes, RefAttributes } from 'react';
+import { FC, forwardRef, HTMLAttributes, RefAttributes } from 'react';
 import Text from './Text';
 import cn from 'classnames';
 
@@ -9,16 +9,18 @@ interface Props extends HTMLAttributes<HTMLDivElement>, RefAttributes<HTMLDivEle
     className?: string;
 }
 
-const Toast: FC<Props> = ({ toast: t, text, className = '', ...props }, ref) => {
+const Toast: FC<Props> = forwardRef<HTMLDivElement, Props>(({ toast: t, text, className = '', ...props }, ref) => {
+    const { id, visible } = t;
+
     return (
         <div
             ref={ref}
             {...props}
             className={cn(
-                `relative flex items-center w-fit rounded-lg overflow-hidden border-2 border-purple bg-gray ${className}`,
+                `relative flex items-center w-full sm:w-fit rounded-lg overflow-hidden border-2 border-purple bg-gray ${className}`,
                 {
-                    'animate-leave': !t.visible,
-                    'animate-enter': t.visible,
+                    'animate-leave': !visible,
+                    'animate-enter': visible,
                 }
             )}
         >
@@ -26,13 +28,14 @@ const Toast: FC<Props> = ({ toast: t, text, className = '', ...props }, ref) => 
 
             <button
                 type='button'
-                onClick={() => toast.dismiss(t.id)}
+                onClick={() => toast.dismiss(id)}
                 className='w-fit h-full p-2.5 outline-none border-l-2 border-purple transition-colors duration-200 hover:bg-purple'
             >
                 Close
             </button>
         </div>
     );
-};
+});
 
+Toast.displayName = 'Toast';
 export default Toast;
